a80c6aa21c264838bf8c3e60dc9501866b05ba49
SamarV-121 <samarvispute121@gmail.com>
2021-04-12 17:03:44 +0000

    Add RMX2001_11.B.59_0590_202103130007

--- vendor/bin/oppo-install-recovery.sh
+++ vendor/bin/oppo-install-recovery.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
-if ! applypatch --check EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:e0dc99bab3b1b5fb15e23382bbe9f926424fb5b5; then
+if ! applypatch --check EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:cf25d36eea5dd1eaa17494b27591c309e712a382; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/platform/bootdevice/by-name/boot:33554432:558ae49ea74c4b49e6f1a16bd421aa2766b738e0 \
-          --target EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:e0dc99bab3b1b5fb15e23382bbe9f926424fb5b5 && \
+          --source EMMC:/dev/block/platform/bootdevice/by-name/boot:33554432:7b991ec26405c0e73c4813d6f2c354c7834f63dc \
+          --target EMMC:/dev/block/platform/bootdevice/by-name/recovery:67108864:cf25d36eea5dd1eaa17494b27591c309e712a382 && \
       log -t recovery "Installing new oppo recovery image: succeeded" && \
       setprop ro.recovery.updated true || \
       log -t recovery "Installing new oppo recovery image: failed" && \
